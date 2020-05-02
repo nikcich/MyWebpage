e08298d0bbbe1f27f9d1165d6bc4a250c3249d79
nikcich <64556428+nikcich@users.noreply.github.com>
2020-05-01 23:40:54 -0500

    removed bad neural net

--- Neuron.js
+++
@@ -1,9 +0,0 @@
-class Neuron{ // contains connections??
-  constructor(){
-    this.Value = 0; // the neurons 'output' value
-  }
-
-  input(x){
-    this.Value = x; // assigns value to the neurons value
-  }
-}
